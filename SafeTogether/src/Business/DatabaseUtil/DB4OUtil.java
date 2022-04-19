2407ca6fd5cf8a7fe6c3e1576d39da7675f8c598
Arjun Raja Yogidas <arjunbirju@Arjuns-MacBook-Air.local>
2022-04-19 14:56:39 -0400

    merge conflicts

--- SafeTogether/src/Business/DatabaseUtil/DB4OUtil.java
+++ SafeTogether/src/Business/DatabaseUtil/DB4OUtil.java
@@ -69,15 +69,15 @@ public class DB4OUtil {
     
     public EcoSystem retrieveSystem(){
         ObjectContainer conn = createConnection();
-//        ObjectSet<EcoSystem> systems = conn.query(EcoSystem.class); // Change to the object you want to save
+        ObjectSet<EcoSystem> systems = conn.query(EcoSystem.class); // Change to the object you want to save
         EcoSystem system;
-        system = SystemConfig.SysConfigure();
-//        if (systems.size() == 0){
-//            system = ConfigureASystem.configure();  // If there's no System in the record, create a new one
-//        }
-//        else{
-//            system = systems.get(systems.size() - 1);
-//        }
+//        system = SystemConfig.SysConfigure();
+        if (systems.isEmpty()){
+            system = SystemConfig.SysConfigure();  // If there's no System in the record, create a new one
+        }
+        else{
+            system = systems.get(systems.size() - 1);
+        }
         conn.close();
         return system;
     }
