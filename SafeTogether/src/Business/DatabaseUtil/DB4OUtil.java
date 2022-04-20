d55ff8c5f5838c379daeb4fff266ee469ab0104e
Arjun Raja Yogidas <arjunbirju@Arjuns-MacBook-Air.local>
2022-04-20 08:34:08 -0400

    changes to make db4o work

--- SafeTogether/src/Business/DatabaseUtil/DB4OUtil.java
+++ SafeTogether/src/Business/DatabaseUtil/DB4OUtil.java
@@ -24,64 +24,57 @@ import com.db4o.ta.TransparentPersistenceSupport;
  */
 public class DB4OUtil {
     private static final String FILENAME = Paths.get("Databank.db4o").toAbsolutePath().toString();// path to the data store
-    public static DB4OUtil dB4OUtil;
-    
+    private static DB4OUtil dB4OUtil;
     public synchronized static DB4OUtil getInstance(){
         if (dB4OUtil == null){
             dB4OUtil = new DB4OUtil();
         }
         return dB4OUtil;
     }
-
     protected synchronized static void shutdown(ObjectContainer conn) {
         if (conn != null) {
             conn.close();
         }
     }
-
-    public ObjectContainer createConnection() {
-        System.out.println("in create connection");
+    private ObjectContainer createConnection() {
         try {
             EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
-            System.out.println("config" + config);
             config.common().add(new TransparentPersistenceSupport());
             //Controls the number of objects in memory
             config.common().activationDepth(Integer.MAX_VALUE);
             //Controls the depth/level of updation of Object
             config.common().updateDepth(Integer.MAX_VALUE);
-
             //Register your top most Class here
             config.common().objectClass(EcoSystem.class).cascadeOnUpdate(true); // Change to the object you want to save
-            System.out.println("Filename: "+ FILENAME);
             ObjectContainer db = Db4oEmbedded.openFile(config, FILENAME);
             return db;
-        } catch (DatabaseFileLockedException | DatabaseReadOnlyException | Db4oIOException | IncompatibleFileFormatException | OldFormatException ex) {
-            System.out.println(ex.getCause());
-            System.out.println(ex.getMessage());
+        } catch (Exception ex) {
+            System.out.print(ex.getMessage());
         }
         return null;
     }
-
     public synchronized void storeSystem(EcoSystem system) {
         ObjectContainer conn = createConnection();
         conn.store(system);
         conn.commit();
         conn.close();
     }
-    
     public EcoSystem retrieveSystem(){
-        ObjectContainer conn = createConnection();
-        System.out.println("retrieveSystem" + conn);
-        ObjectSet<EcoSystem> systems = conn.query(EcoSystem.class); // Change to the object you want to save
-        EcoSystem system;
-//        system = SystemConfig.SysConfigure();
-        if (systems.isEmpty()){
-            system = SystemConfig.SysConfigure();  // If there's no System in the record, create a new one
+        try{
+             ObjectContainer conn = createConnection();
+            ObjectSet<EcoSystem> systems = conn.query(EcoSystem.class); // Change to the object you want to save
+            EcoSystem system;
+            if (systems.size() == 0){
+                system = SystemConfig.SysConfigure();  // If there’s no System in the record, create a new one
+            }
+            else{
+                system = systems.get(systems.size() - 1);
+            }
+            conn.close();
+            return system;
+        } catch(Exception ex) {
+            System.out.print(ex.getMessage());
         }
-        else{
-            system = systems.get(systems.size() - 1);
-        }
-        conn.close();
-        return system;
+        return null;
     }
 }
